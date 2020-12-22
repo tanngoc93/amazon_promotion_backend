3f3448aaa2c9a15bcc994126fdf599d9fb98abe7
Ngoc Nguyen Tan <tanngoc93@users.noreply.github.com>
2020-12-22 14:26:33 +0700

    Update production.sh

--- config/docker/bash/production.sh
+++ config/docker/bash/production.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 set -e
 
-echo
 echo "=> Platform: $PLATFORM"
 
 if [ "$PLATFORM" = "SIDEKIQ" ]; then
   bundle exec sidekiq -C config/sidekiq.yml -e $RAILS_ENV
 else
-  echo
-  echo "Database migration"
-  bundle exec rake db:migrate
-  echo
-  echo "Seed data migration"
-  bundle exec rake seed:migrate
-  echo
-  passenger start
+  bundle exec rake db:migrate seed:migrate && passenger start
 fi
+
+exec "$@"
