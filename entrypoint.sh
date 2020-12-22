6c9d4e963f2dd464c234978260e54a4725bbeacc
Ngoc Nguyen Tan <tanngoc93@users.noreply.github.com>
2020-12-22 14:30:53 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Remove a potentially pre-existing passenger.pid for Rails.
-rm -f ${APP_DIR}/tmp/pids/passenger.pid
+rm -f $APP_DIR/tmp/pids/passenger.pid
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
