6eb1f2400e2d412232b1281820cfb8f821cbc461
Ngoc Nguyen Tan <tanngoc93@users.noreply.github.com>
2020-12-22 14:25:48 +0700

    Update development.sh

--- config/docker/bash/development.sh
+++ config/docker/bash/development.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -e
 
-echo
 echo "=> Platform: $PLATFORM"
 
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f ${APP_DIR}/tmp/pids/server.pid
+rm -f $APP_DIR/tmp/pids/server.pid
 
 #
 bundle check || bundle install
@@ -13,18 +12,7 @@ bundle check || bundle install
 if [ "$PLATFORM" = "SIDEKIQ" ]; then
   bundle exec sidekiq -C config/sidekiq.yml -e $RAILS_ENV
 else
-  echo
-  echo "Create database"
-  bundle exec rake db:create
-  echo
-  echo "Database migration"
-  bundle exec rake db:migrate
-  echo
-  echo "Seed data migration"
-  bundle exec rake seed:migrate
-  echo
-  bundle exec rails s -b 0.0.0.0 -p $PORT
+  bundle exec rake db:create db:migrate seed:migrate && bundle exec puma -C config/puma.rb
 fi
 
-
-
+exec "$@"
